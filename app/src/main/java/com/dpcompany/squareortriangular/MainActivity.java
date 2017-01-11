595f1f9c20e4498fb77d028a6700108059fc85fc
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-01-11 20:10:17 +0530

    Updated Java Code

--- app/src/main/java/com/dpcompany/squareortriangular/MainActivity.java
+++ app/src/main/java/com/dpcompany/squareortriangular/MainActivity.java
@@ -2,8 +2,16 @@ package com.dpcompany.squareortriangular;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void testNumber(View view){
+        EditText usersNumber = (EditText)findViewById(R.id.usersNumber);
+        Log.i("usersNumber", usersNumber.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
