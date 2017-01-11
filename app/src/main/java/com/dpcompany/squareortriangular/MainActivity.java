cb6c97cd09c9832d898d672d1ceec2f4f807a40d
Durgaprasad Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2017-01-11 20:35:52 +0530

    Complete Java Code

--- app/src/main/java/com/dpcompany/squareortriangular/MainActivity.java
+++ app/src/main/java/com/dpcompany/squareortriangular/MainActivity.java
@@ -5,12 +5,65 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    class Number{
+        int number;
+
+        public boolean isSquare(){
+            double squareRoot = Math.sqrt(number);
+            if(squareRoot == Math.floor(squareRoot)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+            }
+
+        public boolean isTriangular(){
+            int x = 1;
+            int triangularNumber = 1;
+            while(triangularNumber < number){
+                x++;
+                triangularNumber = triangularNumber + x;
+            }
+            if(triangularNumber == number){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        }
 
     public void testNumber(View view){
         EditText usersNumber = (EditText)findViewById(R.id.usersNumber);
         Log.i("usersNumber", usersNumber.getText().toString());
+
+        Number myNumber = new Number();
+        myNumber.number = (Integer.parseInt(usersNumber.getText().toString()));
+        String message = "";
+        if(myNumber.isSquare()){
+            if(myNumber.isTriangular()){
+                message = "The number is both triangular and square";
+            }
+            else{
+                message = "The number is a square number";
+            }
+        }
+        else{
+            if(myNumber.isTriangular()){
+                message = "The number is a triangular number";
+            }
+            else{
+                message = "The number is not triangular as well as square";
+            }
+        }
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        System.out.println(myNumber.isSquare());
     }
 
     @Override
